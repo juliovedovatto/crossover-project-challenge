92a81f4942d960e74200fab8e91883650b1f989f
Julio Vedovatto <juliovedovatto@gmail.com>
2016-11-22 12:01:15 -0200

    Updated docker configuration to remove apache .pid file on startup

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 chown www-data:www-data /app -R
 
+if [ ! -d "$APACHE_RUN_DIR" ]; then
+	mkdir "$APACHE_RUN_DIR"
+	chown $APACHE_RUN_USER:$APACHE_RUN_GROUP "$APACHE_RUN_DIR"
+fi
+if [ -f "$APACHE_PID_FILE" ]; then
+	rm "$APACHE_PID_FILE"
+fi
+
 if [ "$ALLOW_OVERRIDE" = "**False**" ]; then
     unset ALLOW_OVERRIDE
 else
